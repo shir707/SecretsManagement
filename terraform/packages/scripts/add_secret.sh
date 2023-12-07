2045557f2b45558d41570471c908449014fd3bdb
shirfogel <shirfogel@gmail.com>
2023-12-07 14:17:54 +0200

    fix script

--- terraform/packages/scripts/add_secret.sh
+++ terraform/packages/scripts/add_secret.sh
@@ -4,11 +4,11 @@
 new_key=$1
 new_value=$2
 
-secret_path="../../../config/stage/terraform/eu-west-1/secrets.auto.tfvars"
 # Check if the key already exists in the file
   # If the key does not exist, add it to the file
 awk -i inplace -v key="$new_key" -v value="$new_value" \
   'BEGIN { FS = OFS = " = " } $1 == "secrets" { found = 1 } \
   found && /^}/ { print "\t\"" key "\" = \"" value "\",\n"; found = 0 } 1' \
-  ${secret_path}
+  secrets.auto.tfvars
 echo "Added key $new_key to secrets.auto.tfvars"
+
