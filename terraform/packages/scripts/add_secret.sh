e53fc356b072560bd8e6b8533b099ab34861122b
shirfogel <shirfogel@gmail.com>
2023-12-07 14:09:16 +0200

    fix add secret pipeline

--- terraform/packages/scripts/add_secret.sh
+++ terraform/packages/scripts/add_secret.sh
@@ -4,14 +4,11 @@
 new_key=$1
 new_value=$2
 
+secret_path="../../../config/stage/terraform/eu-west-1/secrets.auto.tfvars"
 # Check if the key already exists in the file
-if [[grep -qw "\b$new_key\b" secrets.auto.tfvars]]; then
-  echo "Key $new_key already exists in secrets.auto.tfvars"
-else
   # If the key does not exist, add it to the file
-  awk -i inplace -v key="$new_key" -v value="$new_value" \
-    'BEGIN { FS = OFS = " = " } $1 == "secrets" { found = 1 } \
-    found && /^}/ { print "\t\"" key "\" = \"" value "\",\n"; found = 0 } 1' \
-    secrets.auto.tfvars
-  echo "Added key $new_key to secrets.auto.tfvars"
-fi
+awk -i inplace -v key="$new_key" -v value="$new_value" \
+  'BEGIN { FS = OFS = " = " } $1 == "secrets" { found = 1 } \
+  found && /^}/ { print "\t\"" key "\" = \"" value "\",\n"; found = 0 } 1' \
+  ${secret_path}
+echo "Added key $new_key to secrets.auto.tfvars"
